edb2fca043bdd8292c5991840099580cb9b01fb7
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-08-13 23:04:54 +1000

    Allow $ to retrieve DOM nodes by node reference or selector query

--- minimal-jquery.js
+++ minimal-jquery.js
@@ -1,1 +1,13 @@
-function $() {}
+class NodeCollection extends Array {
+  // jQuery methods will exist in this class, returned by the $ function
+}
+
+function $(selectorOrNode) {
+  if (typeof selectorOrNode === "string") {
+    const selector = selectorOrNode;
+    return new NodeCollection(...document.querySelectorAll(selector));
+  } else {
+    const node = selectorOrNode;
+    return new NodeCollection(node);
+  }
+}
