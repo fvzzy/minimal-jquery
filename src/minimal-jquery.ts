54d4f9d8b50132356de47da63b8dcb3442b59b99
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-08-14 22:03:56 +1000

    Add stricter checks and fix errors

--- src/minimal-jquery.ts
+++ src/minimal-jquery.ts
@@ -1,17 +1,19 @@
-class NodeCollection extends Array {
-  width(value) {
+class NodeCollection extends Array<HTMLElement> {
+  width(value: number | undefined) {
     if (!value) return this[0].offsetWidth;
-    this.forEach((node) => (node.offsetWidth = value));
+    this.forEach((node) => (node.style.width = `${value}px`));
     return value; // I'm only guessing this is what passing a new width would return
   }
 }
 
-function $(selectorOrNode) {
+function $(selectorOrNode: String | HTMLElement) {
   if (typeof selectorOrNode === "string") {
     const selector = selectorOrNode;
-    return new NodeCollection(...document.querySelectorAll(selector));
+    return new NodeCollection(
+      ...document.querySelectorAll<HTMLElement>(selector)
+    );
   } else {
     const node = selectorOrNode;
-    return new NodeCollection(node);
+    return new NodeCollection(node as HTMLElement);
   }
 }
