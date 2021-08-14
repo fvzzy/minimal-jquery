21577ced8289d62337b936cbc1c2a0609d53089e
Farhan Rahman <fvzzy@users.noreply.github.com>
2021-08-14 22:18:40 +1000

    Rename param to match jQuery docs

--- src/minimal-jquery.ts
+++ src/minimal-jquery.ts
@@ -1,19 +1,19 @@
-class NodeCollection extends Array<HTMLElement> {
+class ElementCollection extends Array<HTMLElement> {
   width(value: number | undefined) {
     if (!value) return this[0].offsetWidth;
-    this.forEach((node) => (node.style.width = `${value}px`));
+    this.forEach((element) => (element.style.width = `${value}px`));
     return value; // I'm only guessing this is what passing a new width would return
   }
 }
 
-function $(selectorOrNode: String | HTMLElement) {
-  if (typeof selectorOrNode === "string") {
-    const selector = selectorOrNode;
-    return new NodeCollection(
+function $(selectorOrElement: String | HTMLElement) {
+  if (typeof selectorOrElement === "string") {
+    const selector = selectorOrElement;
+    return new ElementCollection(
       ...document.querySelectorAll<HTMLElement>(selector)
     );
   } else {
-    const node = selectorOrNode;
-    return new NodeCollection(node as HTMLElement);
+    const element = selectorOrElement;
+    return new ElementCollection(element as HTMLElement);
   }
 }
